f0ba186431913acfa2a89dad887deeaa740ab5c1
Nokuthula Mahlangu <nokuthulamargaret@gmail.com>
2025-03-14 08:34:42 +0000

    Plotting data using linear functions

--- Plotting Linear Functions/plot_data_to_standard_out.py
+++ Plotting Linear Functions/plot_data_to_standard_out.py
@@ -1,4 +1,3 @@
-#Three lines to make our compiler able to draw:
 import sys
 import matplotlib
 matplotlib.use('Agg')
@@ -6,14 +5,16 @@ matplotlib.use('Agg')
 import pandas as pd
 import matplotlib.pyplot as plt
 
-health_data = pd.read_csv("data.csv", header=0, sep=",")
+health_data = pd.read_csv("existing_data.csv", header=0, sep=",")
 
 health_data.plot(x ='Average_Pulse', y='Calorie_Burnage', kind='line')
 plt.ylim(ymin=0)
 plt.xlim(xmin=0)
 
-plt.show()
+"""
+When outputting to standard out, the output will be binary data. 
+If you are viewing the output in a terminal, it will appear as garbled text.
+"""
 
-#Two lines to make our compiler able to draw:
 plt.savefig(sys.stdout.buffer)
 sys.stdout.flush()
