388ad46f1d2de6161b8735fb16153f0153494f29
Thulibathi <nokuthulamargaret@gmail.com>
2025-03-20 09:18:49 +0200

    Mathematical functions

---
+++ DS Math/DS Slope and Intercept/find_the_slope_and_intercept.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+health_data = pd.read_csv("slope_and_intercept_data.csv", header=0, sep=",")
+
+x = health_data["Average_Pulse"]
+y = health_data["Calorie_Burnage"]
+slope_intercept = np.polyfit(x,y,1)
+
+print(slope_intercept)
---
+++ DS Math/DS Slope and Intercept/plot_to_file.py
@@ -0,0 +1,22 @@
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+health_data = pd.read_csv("slope_and_intercept_data.csv", header=0, sep=",")
+
+health_data.plot(x ='Average_Pulse', y='Calorie_Burnage', kind='line'),
+plt.ylim(ymin=0, ymax=400)
+plt.xlim(xmin=0, xmax=150)
+
+plt.show()
+
+#Two lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
+#Plot to a file named health_plot.png
+plt.savefig("health_plot_.png") # 
